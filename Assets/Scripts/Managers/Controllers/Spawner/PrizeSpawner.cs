7f90c0f67dde9f08ce17dca85436dc5f54685159
Tolga-dev <orhan.konat.12@gmail.com>
2024-10-09 02:28:14 +0300

    Prize Spawner

--- Assets/Scripts/Managers/Controllers/Spawner/PrizeSpawner.cs
+++ Assets/Scripts/Managers/Controllers/Spawner/PrizeSpawner.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using Save.GameObjects.Road;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Managers.Controllers.Spawner
 {
@@ -8,61 +10,62 @@ namespace Managers.Controllers.Spawner
     public class PrizeSpawner
     {
         public List<GameObject> prizes = new List<GameObject>();
+        public GameObject chest;
+        
         public List<GameObject> createdPrizes = new List<GameObject>();
-
+        
+        private int chestSpawnCount = 0; 
+        private const int maxChestSpawns = 3;
+        
         public void SpawnObject(SpawnerManager spawnerManager)
         {
-            var createdRoads = spawnerManager.roadSpawner.createdRoads;
-            var createdCircleRoads = spawnerManager.roadSpawner.createdCircleRoads;
-
-            foreach (var road in createdRoads)
+            SpawnPrizes(spawnerManager.roadSpawner.createdRoads);
+            SpawnPrizes(spawnerManager.roadSpawner.createdCircleRoads);
+            
+            if (chestSpawnCount < maxChestSpawns)
             {
-                for (int i = 0; i < road.isObjSpawned.Count; i++)
-                {
-                    var isSpawned = road.isObjSpawned[i];
-
-                    if (isSpawned == false)
-                    {
-                        var spawnPoints = road.spawnPoint;
-
-                        var selectedSpawnPoint = spawnPoints[i];
-
-                        var spawn = prizes[0]; // You can adjust this to select a random prize if needed
-
-                        var created = UnityEngine.Object.Instantiate(spawn, selectedSpawnPoint, true);
-                        AdjustPositionToSpawnPoint(created, selectedSpawnPoint);
-
-                        createdPrizes.Add(created);
-                    }
-                }
-            }
-
-            foreach (var circleRoad in createdCircleRoads)
-            {
-                foreach (var spawnPoint in circleRoad.spawnPoint)
-                {
-                    var selectedSpawnPoint = spawnPoint;
-
-                    var spawn = prizes[0]; // You can adjust this to select a random prize if needed
-
-                    var created = UnityEngine.Object.Instantiate(spawn, selectedSpawnPoint, true);
-                    AdjustPositionToSpawnPoint(created, selectedSpawnPoint);
-
-                    createdPrizes.Add(created);
-                }
+                SpawnChest(spawnerManager);
+                // chestSpawnCount++; // when game is finished
             }
             
         }
 
-        private void AdjustPositionToSpawnPoint(GameObject created, Transform selectedSpawnPoint)
+        private void SpawnPrizes(IEnumerable<Road> roads)
         {
-            var position = created.transform.position;
-            var spawnPosition = selectedSpawnPoint.position;
+            foreach (var road in roads)
+            {
+                for (int i = 0; i < road.spawnPoint.Count; i++)
+                {
+                    if (!road.isObjSpawned[i])
+                    {
+                        CreatePrize(road.spawnPoint[i]);
+                    }
+                }
+            }
+        }
 
-            position.x = spawnPosition.x;
-            position.z = spawnPosition.z;
-
-            created.transform.position = position;
+        private void CreatePrize(Transform spawnPoint)
+        {
+            var rate = Random.Range(0, 200);
+            var index = rate > 100 ? 0 : rate > 12.5 ? 1 : 2;
+            
+            var prizePrefab = prizes[index];  
+            var prize = UnityEngine.Object.Instantiate(prizePrefab, spawnPoint, true);
+            var position = spawnPoint.position;
+            
+            prize.transform.position = new Vector3(position.x, prize.transform.position.y, position.z);
+            createdPrizes.Add(prize);
+        }
+        private void SpawnChest(SpawnerManager spawnerManager)
+        {
+            var road = spawnerManager.roadSpawner.createdRoads[0]; // make it easy some times :)
+            var spawnPoint = road.spawnPoint[Random.Range(0, road.spawnPoint.Count)];
+            SpawnAtPoint(chest, spawnPoint);
+        }
+        private void SpawnAtPoint(GameObject prize, Transform spawnPoint)
+        {
+            var created = UnityEngine.Object.Instantiate(prize, spawnPoint.position, Quaternion.identity);
+            createdPrizes.Add(created);
         }
     }
 }
