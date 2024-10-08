c60d30fdbc3a4a68358bd249e969f8215d268ffd
Tolga-dev <orhan.konat.12@gmail.com>
2024-10-08 17:11:51 +0300

    Adding more obstable

---
+++ Assets/Scripts/Save/GameObjects/Obstacle/RotatingPlatform.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    [SerializeField] float rotatingSpeed = 10f;
+    [SerializeField] Vector3 rotatingAxis;
+
+    void Update()
+    {
+        transform.Rotate(rotatingAxis, rotatingSpeed * Time.deltaTime);
+    }
+}
