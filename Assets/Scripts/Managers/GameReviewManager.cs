bce144a667ce071d2fd02adcd7e995c6ca223b9c
Tolga-dev <orhan.konat.12@gmail.com>
2024-10-11 16:13:12 +0300

    Adding review

--- Assets/Scripts/Managers/GameReviewManager.cs
+++ Assets/Scripts/Managers/GameReviewManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Google.Play.Review;
 using UnityEngine;
 
@@ -6,18 +7,43 @@ namespace Managers
 {
     public class GameReviewManager : MonoBehaviour
     {
-        private ReviewManager _gameReviewManager;
+        private ReviewManager _reviewManager;
+        private PlayReviewInfo _playReviewInfo;
 
-        public void Start()
+        private void Start()
         {
-            _gameReviewManager = new ReviewManager();
-            
+            _reviewManager = new ReviewManager();
         }
-        
-        public void RewardUser()
+
+        public void PopUp()
         {
-            
+            StartCoroutine(InitiateReviewFlow());
         }
-        
+
+        private IEnumerator InitiateReviewFlow()
+        {
+            var requestFlowOperation = _reviewManager.RequestReviewFlow();
+            yield return requestFlowOperation;
+
+            if (requestFlowOperation.Error != ReviewErrorCode.NoError)
+            {
+                Debug.LogError("Failed to request review flow: " + requestFlowOperation.Error.ToString());
+                yield break; // Exit if there's an error requesting the review flow.
+            }
+
+            _playReviewInfo = requestFlowOperation.GetResult();
+
+            var launchFlowOperation = _reviewManager.LaunchReviewFlow(_playReviewInfo);
+            yield return launchFlowOperation;
+
+            if (launchFlowOperation.Error != ReviewErrorCode.NoError)
+            {
+                Debug.LogError("Failed to launch review flow: " + launchFlowOperation.Error.ToString());
+                yield break; // Exit if there's an error launching the review flow.
+            }
+
+            Debug.Log("Review flow completed successfully.");
+            _playReviewInfo = null; // Clear the PlayReviewInfo object after the review flow completes.
+        }
     }
 }
